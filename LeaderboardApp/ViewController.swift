fd725c0e14a086c8505d0d80326d9d4c2c454f07
AndyLin <croc0909@gmail.com>
2022-08-17 09:37:25 +0800

    Initial Commit

---
+++ LeaderboardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeaderboardApp
+//
+//  Created by AndyLin on 2022/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
